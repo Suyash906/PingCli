1d1ee8b0f37ccae8b9ee5240995d334cdc4545c0
Suyash906 <suyash90682711@gmail.com>
2020-04-19 20:36:26 -0700

    icmp echo ping ipv4 added

---
+++ cmd/pinger.go
@@ -0,0 +1,142 @@
+/*
+Copyright © 2020 NAME HERE <EMAIL ADDRESS>
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+package cmd
+
+import (
+	"fmt"
+
+	"github.com/spf13/cobra"
+	"time"
+	"os"
+	"log"
+	"net"
+	"golang.org/x/net/icmp"
+	"golang.org/x/net/ipv4"
+)
+
+// pingerCmd represents the pinger command
+var pingerCmd = &cobra.Command{
+	Use:   "pinger",
+	Short: "A brief description of your command",
+	Long: `A longer description that spans multiple lines and likely contains examples
+and usage of using your command. For example:
+
+Cobra is a CLI library for Go that empowers applications.
+This application is a tool to generate the needed files
+to quickly create a Cobra application.`,
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println("pinger called")
+		fmt.Println(args)
+		p := func(addr string){
+			dst, dur, err := PingUtility(addr)
+			if err != nil {
+				log.Printf("Ping %s (%s): %s\n", addr, dst, err)
+				return
+			}
+			log.Printf("Ping %s (%s): %s\n", addr, dst, dur)
+		}
+		for{
+			p(args[0])
+			time.Sleep(2 * time.Second)
+		}
+	},
+}
+
+var ListenAddr = "0.0.0.0"
+
+const (
+    // Stolen from https://godoc.org/golang.org/x/net/internal/iana,
+    // can't import "internal" packages
+    ProtocolICMP = 1
+    //ProtocolIPv6ICMP = 58
+)
+
+func PingUtility(addr string)  (*net.IPAddr, time.Duration, error) {
+	fmt.Println(`PingUtility`)
+	// Start listening for icmp replies
+    c, err := icmp.ListenPacket("ip4:icmp", ListenAddr)
+    if err != nil {
+        return nil, 0, err
+    }
+    defer c.Close()
+
+    // Resolve any DNS (if used) and get the real IP of the target
+    dst, err := net.ResolveIPAddr("ip4", addr)
+    if err != nil {
+        panic(err)
+        return nil, 0, err
+    }
+
+    // Make a new ICMP message
+    m := icmp.Message{
+        Type: ipv4.ICMPTypeEcho, Code: 0,
+        Body: &icmp.Echo{
+            ID: os.Getpid() & 0xffff, Seq: 1, //<< uint(seq), // TODO
+            Data: []byte(""),
+        },
+    }
+    b, err := m.Marshal(nil)
+    if err != nil {
+        return dst, 0, err
+    }
+
+    // Send it
+    start := time.Now()
+    n, err := c.WriteTo(b, dst)
+    if err != nil {
+        return dst, 0, err
+    } else if n != len(b) {
+        return dst, 0, fmt.Errorf("got %v; want %v", n, len(b))
+    }
+
+    // Wait for a reply
+    reply := make([]byte, 1500)
+    err = c.SetReadDeadline(time.Now().Add(10 * time.Second))
+    if err != nil {
+        return dst, 0, err
+    }
+    n, peer, err := c.ReadFrom(reply)
+    if err != nil {
+        return dst, 0, err
+    }
+    duration := time.Since(start)
+
+    // Pack it up boys, we're done here
+    rm, err := icmp.ParseMessage(ProtocolICMP, reply[:n])
+    if err != nil {
+        return dst, 0, err
+    }
+    switch rm.Type {
+    case ipv4.ICMPTypeEchoReply:
+        return dst, duration, nil
+    default:
+        return dst, 0, fmt.Errorf("got %+v from %v; want echo reply", rm, peer)
+    }
+}
+
+func init() {
+	rootCmd.AddCommand(pingerCmd)
+
+	// Here you will define your flags and configuration settings.
+
+	// Cobra supports Persistent Flags which will work for this command
+	// and all subcommands, e.g.:
+	// pingerCmd.PersistentFlags().String("foo", "", "A help for foo")
+
+	// Cobra supports local flags which will only run when this command
+	// is called directly, e.g.:
+	// pingerCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
--- main.go
+++ main.go
@@ -15,7 +15,7 @@ limitations under the License.
 */
 package main
 
-import "github.com/spf13/PingCli/cmd"
+import "./cmd"
 
 func main() {
 	cmd.Execute()
